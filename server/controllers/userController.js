ea2f3d4aaf08fc46ee82f50cbd106484f9244120
Soham Daga <soham.daga22@gmail.com>
2024-10-03 15:56:52 -0700

    Added image compression

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -14,9 +14,9 @@ const vectorStore = new MongoDBAtlasVectorSearch(
   embeddings,
   {
     collection: User.db.collection("users"), 
-    indexName: "vector_index", // The name of the Atlas search index. Defaults to "default"
-    textKey: "summary", // The name of the collection field containing the raw content. Defaults to "text"
-    embeddingKey: "embedding", // The name of the collection field containing the embedded text. Defaults to "embedding"
+    indexName: "vector_index",
+    textKey: "summary",
+    embeddingKey: "embedding",
   }
 );
 
@@ -111,10 +111,10 @@ const setUserStatus = async (req, res, next) => {
   const duration = req.body.duration;
   const expirationDate = 
     duration == "24h" ? 
-    new Date(Date.now() + 60 * 1000) : (
+    new Date(Date.now() + 24 * 60 * 60 * 1000) : (
       duration == "48h" ? 
-      new Date(Date.now() + 2 * 60 * 1000) :
-      new Date(Date.now() + 7 * 60 * 1000)
+      new Date(Date.now() + 2 * 24 * 60 * 60 * 1000) :
+      new Date(Date.now() + 7 * 24 * 60 * 60 * 1000)
     );
   const status = {
     content: req.body.status,
