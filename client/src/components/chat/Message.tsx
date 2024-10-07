0dd8be954f6895ad629ae1a0238d4abf2d495984
Soham Daga <soham.daga22@gmail.com>
2024-10-07 15:08:31 -0700

    Initial refactor

--- client/src/components/chat/Message.tsx
+++ client/src/components/chat/Message.tsx
@@ -8,15 +8,18 @@ interface MessageProps {
 }
 
 const Message: React.FC<MessageProps> = ({ content, timestamp, isSender }) => {
+  const alignmentClass = isSender ? "justify-end" : "justify-start"
+  const bgColorClass = isSender ? "bg-orange-500" : "bg-gray-600"
+
   return (
-    <div className={`flex ${isSender ? "justify-end" : "justify-start"}`}>
+    <div className={`flex ${alignmentClass}`}>
         <div
-            className={`flex flex-col gap-1 max-w-xs p-2 rounded-lg text-white ${isSender ? "bg-orange-500" : "bg-gray-600"}`}
+            className={`flex flex-col gap-1 max-w-xs p-2 rounded-lg text-white ${bgColorClass}`}
         >
-            {content}
-            <div className="text-xs text-gray-200 place-self-end">
+            <p>{content}</p>
+            <time className="text-xs text-gray-200 place-self-end">
                 {formatTime(timestamp)}
-            </div>
+            </time>
         </div>
     </div>
   )
