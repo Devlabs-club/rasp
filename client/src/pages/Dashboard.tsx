be84f65ef16812dc6b86808323c8f76f86ef2805
Soham Daga <soham.daga22@gmail.com>
2024-10-11 12:53:13 -0700

    Fixed logout issue

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -25,6 +25,7 @@ const Dashboard: React.FC = () => {
   const Logout = useCallback(() => {
     setUser(null);
     googleLogout();
+    document.cookie = "token=; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT";
     navigate("/signin");
   }, [navigate, setUser]);
 
